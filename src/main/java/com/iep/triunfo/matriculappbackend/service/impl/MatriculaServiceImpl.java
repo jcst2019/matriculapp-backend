f7f871b1d2f16435c85c4415d8c13e984628244c
jcst2019 <juansolar4@gmail.com>
2022-04-02 12:02:17 -0500

    Generar PDF constacia Matriucla-Error class path resource [ConstanciaMatricula.jasper] cannot be resolved to absolute file path because it does not reside in the file system(4)

--- src/main/java/com/iep/triunfo/matriculappbackend/service/impl/MatriculaServiceImpl.java
+++ src/main/java/com/iep/triunfo/matriculappbackend/service/impl/MatriculaServiceImpl.java
@@ -15,7 +15,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -59,7 +61,7 @@ public class MatriculaServiceImpl implements IMatriculaService {
         byte[] data = null;
 
         try {
-            File file = new ClassPathResource("ConstanciaMatricula.jasper").getFile();
+            File file = new ClassPathResource("/reports/ConstanciaMatricula.jasper").getFile();//Funciona Pero en el despliegue NO. No encuentra la ruta de la carpeta resource
             JasperPrint print = JasperFillManager.fillReport(file.getPath(), null, new JRBeanCollectionDataSource(Collections.singleton(this.listarPorId(id))));
             data = JasperExportManager.exportReportToPdf(print);
         }catch(Exception e) {
